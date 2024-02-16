83a74c012b67db46eaa5551ac46486b2b1e3ab6e
AminaSagheer <144405367+AminaSagheer@users.noreply.github.com>
2024-02-15 18:19:43 -0700

    Update Inquirer.java

--- MyClasses/Inquirer.java
+++ MyClasses/Inquirer.java
@@ -1,4 +1,7 @@
-package MyClasses;
+package edu.ucalgary.oop;
+
+import org.junit.*;
+
 
 public class Inquirer {
     private String FIRST_NAME;
@@ -6,18 +9,26 @@ public class Inquirer {
     private String INFO;
     private String SERVICES_PHONE;
 
-    /* Constructors */
-    public Inquirer(String FIRST_NAME, String LAST_NAME, String INFO
-    String SERVICES_PHONE){
-        this.setFIRST_NAME();
-        this.setLAST_NAME();
-        this.setINFO();
-        this.setSERVICES_PHONE();
+    public Inquirer(String FIRST_NAME, String LAST_NAME, String SERVICES_PHONE, String INFO) {
+        this.FIRST_NAME = FIRST_NAME;
+        this.LAST_NAME = LAST_NAME;
+        this.SERVICES_PHONE = SERVICES_PHONE;
+        this.INFO = INFO;
     }
 
-    /* Getters */
-    public String getFirstName() {return this.FIRST_NAME;}
-    public String getLastName() {return this.LAST_NAME;}
-    public String getInfo() {return this.INFO;}
-    public String getServicesPhone() {return this.SERVICES_PHONE;}
+    public String getFirstName() {
+        return FIRST_NAME;
+    }
+
+    public String getLastName() {
+        return LAST_NAME;
+    }
+
+    public String getInfo() {
+        return INFO;
+    }
+
+    public String getServicesPhoneNum() {
+        return SERVICES_PHONE;
+    }
 }
